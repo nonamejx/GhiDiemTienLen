a079eeb9589ed8d3987568c643914bae21f44b27
haihb <huynhbahai94@gmail.com>
2016-11-17 00:21:58 +0700

    Implement BarChartResultFragment

--- app/src/main/java/com/nonamejx/ghidiemtienlen/fragment/BarChartResultFragment.java
+++ app/src/main/java/com/nonamejx/ghidiemtienlen/fragment/BarChartResultFragment.java
@@ -8,12 +8,22 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.nonamejx.ghidiemtienlen.R;
+import com.nonamejx.ghidiemtienlen.common.Constants;
 import com.nonamejx.ghidiemtienlen.database.DataCenter;
 import com.nonamejx.ghidiemtienlen.model.Game;
 
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.FragmentArg;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by noname
@@ -41,6 +51,31 @@ public class BarChartResultFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_chart_result, container, false);
         BarChart barChart = (BarChart) v.findViewById(R.id.barChartResult);
+
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+        final int[] finalResults = mGame.calculateFinalResult();
+        for (int i = 0; i < Constants.NUMBER_OF_PLAYERS; i++) {
+            barEntries.add(new BarEntry(finalResults[i], i));
+        }
+        BarDataSet dataSet = new BarDataSet(barEntries, null);
+
+        List<String> labels = Arrays.asList(mGame.getPlayerNames());
+
+        BarData barData = new BarData(labels, dataSet);
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        barChart.setData(barData);
+        barChart.setDoubleTapToZoomEnabled(false);
+        barChart.setScaleEnabled(false);
+        barChart.getAxisRight().setEnabled(false);
+        barChart.getXAxis().setDrawGridLines(false);
+        barChart.setDescription("");
+        barChart.setDrawGridBackground(false);
+        barChart.getLegend().setEnabled(false);
+        barChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+        barChart.getXAxis().setTextSize(12);
+        barChart.animateY(500);
+        barChart.invalidate();
 
         return v;
     }
